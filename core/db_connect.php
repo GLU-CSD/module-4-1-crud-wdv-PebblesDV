39840aff03d18b7fc17442ca30b41ca06e483b73
PebblesDV <pebblesdevries@gmail.com>
2024-04-23 14:13:14 +0200

    fixed connection

--- core/db_connect.php
+++ core/db_connect.php
@@ -4,30 +4,31 @@ session_start();
 /**
  * Voor de MAC gebruikers;
  */
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "root";
-$dbname = "webdev_base";
+// $dbhost = "localhost";
+// $dbuser = "root";
+// $dbpass = "root";
+// $dbname = "producten";
 
 /**
  * Voor de Windows gebruikers;
  */
-// $dbhost = "localhost";
-// $dbuser = "root";
-// $dbpass = "";
-// $dbname = "webdev_base";
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "producten";
 
 $con = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
-if ($con -> connect_errno) {
-    echo "Failed to connect to MySQL: " . $con -> connect_error;
+if ($con->connect_errno) {
+    echo "Failed to connect to MySQL: " . $con->connect_error;
     exit();
 }
 
-define("BASEURL","http://localhost/webdev-base/");
-define("BASEURL_CMS","http://localhost/webdev-base/admin/");
+define("BASEURL", "http://localhost/school/jaar1/periode4/crud/module-4-1-crud-wdv-PebblesDV/");
+// define("BASEURL_CMS", "http://localhost/webdev-base/admin/");
 
-function prettyDump ( $var ) {
+function prettyDump($var)
+{
     echo "<pre>";
     var_dump($var);
     echo "</pre>";
